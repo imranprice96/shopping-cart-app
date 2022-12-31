fc7518788af1442ca73a85676b2b9c057be20f1d
imranprice96 <imranprice96@gmail.com>
2022-12-31 20:51:34 +1300

    Add store api request Add layout for items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-	return <div className="App">Home</div>;
+	return (
+		<div className="App">
+			<h1>Home</h1>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import "./Shop.css";
+
 function Shop() {
-	return <div>Shop</div>;
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		fetchItems();
+	}, []);
+
+	const fetchItems = async () => {
+		const data = await fetch("https://fakestoreapi.com/products?limit=12");
+		const items = await data.json();
+		console.log(items);
+		setItems(items);
+	};
+
+	return (
+		<div>
+			<h1>Shop</h1>
+			<div className="store-items">
+				{items.map((item) => (
+					<div key={item.id} className="item">
+						<h3>{item.title}</h3>
+						<img src={item.image} alt=""></img>
+						<div>
+							<button>+</button>
+							<input
+								type="number"
+								id="quantity"
+								name="quantity"
+								min={1}
+							/>
+							<button>-</button>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Shop;
