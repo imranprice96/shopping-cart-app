73a8a880e9f3b930c9d08c6d44c4fe6f95619d63
imranprice96 <imranprice96@gmail.com>
2023-01-14 20:09:41 +1300

    Add modal for cart

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom";
 import "./Nav.css";
+import Modal from "react-modal";
+import { useState } from "react";
 
 function Nav(props) {
+	let subtitle;
 	const { cart, setCart } = props;
+	const [modalIsOpen, setIsOpen] = useState(false);
+
+	const openModal = () => {
+		setIsOpen(true);
+	};
+
+	const afterOpenModal = () => {
+		subtitle.style.color = "blue";
+	};
+
+	const closeModal = () => {
+		setIsOpen(false);
+	};
+
+	const checkout = () => {
+		setCart([]);
+		closeModal();
+	};
+
 	return (
 		<nav className="navbar">
 			<Link to="/">
@@ -13,9 +35,22 @@ function Nav(props) {
 					<h3>Shop</h3>
 				</Link>
 				<Link>
-					<h3>Cart: {cart.length}</h3>
+					<h3 onClick={openModal}>Cart: {cart.length}</h3>
 				</Link>
 			</div>
+			<Modal
+				isOpen={modalIsOpen}
+				onAfterOpen={afterOpenModal}
+				onRequestClose={closeModal}
+				contentLabel="Example"
+			>
+				<div>
+					<Link to="/">
+						<button onClick={checkout}>Checkout</button>
+					</Link>
+					<button onClick={closeModal}>Continue Shopping</button>
+				</div>
+			</Modal>
 		</nav>
 	);
 }
