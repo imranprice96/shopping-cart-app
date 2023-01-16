46bcc2058e615e19eef46c274b48e968001a52d1
imranprice96 <imranprice96@gmail.com>
2023-01-16 19:28:29 +1300

    Add homepage content and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div className="content">
+				<div id="app-title">Store That Sells Stuff</div>
+				<div id="app-subtitle">Shipping World Wide</div>
+				<button id="app-btn">Shop now</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
